2ed15d43dcbdfdcaf04c785d131d54a67935ecad
sirius4galaxy <uniswap_v2@163.com>
2024-04-30 11:30:58 +0800

    change block interval time to 1 second. 1. change block_interval_ms=1000 2. change default_max_block_net_usage=2 * 1024 * 1024 3. default_net_usage_leeway=1000 4. default_max_block_cpu_usage=400'000 5. producer_repetitions=6

--- libraries/chain/include/eosio/chain/config.hpp
+++ libraries/chain/include/eosio/chain/config.hpp
@@ -36,7 +36,7 @@ const static name eosio_code_name { "flon.code"_n };
 const static name main_shard_name { "main"_n };
 const static name share_db_name { "share"_n };
 
-const static int      block_interval_ms = 500;
+const static int      block_interval_ms = 1000;
 const static int      block_interval_us = block_interval_ms*1000;
 const static uint64_t block_timestamp_epoch = 946684800000ll; // epoch is year 2000.
 const static uint32_t genesis_num_supported_key_types = 2;
@@ -58,16 +58,16 @@ static const uint32_t maximum_elastic_resource_multiplier  = 1000;
 const static uint32_t   rate_limiting_precision        = 1000*1000;
 
 
-const static uint32_t   default_max_block_net_usage                  = 1024 * 1024; /// at 500ms blocks and 200byte trx, this enables ~10,000 TPS burst
+const static uint32_t   default_max_block_net_usage                  = 2 * 1024 * 1024; /// at 1000ms blocks and 200byte trx, this enables ~10,000 TPS burst
 const static uint32_t   default_target_block_net_usage_pct           = 10 * percent_1; /// we target 1000 TPS
 const static uint32_t   default_max_transaction_net_usage            = default_max_block_net_usage / 2;
 const static uint32_t   default_base_per_transaction_net_usage       = 12;  // 12 bytes (11 bytes for worst case of transaction_receipt_header + 1 byte for static_variant tag)
-const static uint32_t   default_net_usage_leeway                     = 500; // TODO: is this reasonable?
+const static uint32_t   default_net_usage_leeway                     = 1000; // TODO: is this reasonable?
 const static uint32_t   default_context_free_discount_net_usage_num  = 20; // TODO: is this reasonable?
 const static uint32_t   default_context_free_discount_net_usage_den  = 100;
 const static uint32_t   transaction_id_net_usage                     = 32; // 32 bytes for the size of a transaction id
 
-const static uint32_t   default_max_block_cpu_usage                  = 200'000; /// max block cpu usage in microseconds
+const static uint32_t   default_max_block_cpu_usage                  = 400'000; /// max block cpu usage in microseconds
 const static uint32_t   default_target_block_cpu_usage_pct           = 10 * percent_1;
 const static uint32_t   default_max_transaction_cpu_usage            = 3*default_max_block_cpu_usage/4; /// max trx cpu usage in microseconds
 const static uint32_t   default_min_transaction_cpu_usage            = 100; /// min trx cpu usage in microseconds (10000 TPS equiv)
@@ -128,7 +128,7 @@ const static uint32_t   default_abi_serializer_max_time_us = 15*1000; ///< defau
 /**
  *  The number of sequential blocks produced by a single producer
  */
-const static int producer_repetitions = 12;
+const static int producer_repetitions = 6;
 const static int max_producers = 125;
 
 const static size_t maximum_tracked_dpos_confirmations = 1024;     ///<
