2cb6cf454feee7651067d3ad7297048997d151ae
sirius4galaxy <uniswap_v2@163.com>
2024-05-08 16:51:03 +0800

    refine pinned build script

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-apt-get update
-apt-get update --fix-missing
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+
+if [[ "X$1" == "X" ]]; then
+    echo "Missing arg: DEP_DIR"
+    echo "./install_deps.sh <DEP_DIR>"
+    exit 1
+fi
+
+# CMAKE_C_COMPILER requires absolute path
+DEP_DIR="$(realpath "$1")"
+APT_CMD=${2:-"apt-get"}
+
+${APT_CMD} update
+${APT_CMD} update --fix-missing
 export DEBIAN_FRONTEND='noninteractive'
 export TZ='Etc/UTC'
-apt-get install -y \
+${APT_CMD} install -y \
     build-essential \
     bzip2 \
     cmake \
@@ -25,3 +38,8 @@ apt-get install -y \
     wget \
     zip \
     zlib1g-dev
+
+source "${SCRIPT_DIR}/utils.sh"
+install_clang "${DEP_DIR}/clang-${CLANG_VER}"
+install_llvm "${DEP_DIR}/llvm-${LLVM_VER}"
+install_boost "${DEP_DIR}/boost_${BOOST_VER//\./_}patched"
